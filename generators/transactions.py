2b09494e965b8ce90aef8973facfcd03dfa03912
GarvSheth <b23es1016@iitj.ac.in>
2026-07-03 16:17:15 +0530

    added new features in transaction and made it faster by adding dict

--- generators/transactions.py
+++ generators/transactions.py
@@ -19,6 +19,19 @@ devices = pd.read_csv("output/devices.csv")
 merchants = pd.read_csv("output/merchants.csv")
 locations = pd.read_csv("output/locations.csv")
 
+# Faster lookups
+
+user_list = users.to_dict("records")
+
+merchant_list = merchants.to_dict("records")
+
+location_list = locations.to_dict("records")
+
+device_lookup = {}
+
+for user_id, group in devices.groupby("UserID"):
+    device_lookup[user_id] = group.to_dict("records")
+
 # ===========================
 # Transaction Types
 # ===========================
@@ -55,21 +68,23 @@ for _, user in users.iterrows():
 def generate_transaction():
 
     # Random User
-    user = users.sample(1).iloc[0]
+    user = random.choice(user_list)
     user_id = user["UserID"]
 
     # Devices owned by this user
-    user_devices = devices[
-        devices["UserID"] == user_id
-    ]
-
-    device = user_devices.sample(1).iloc[0]
+    device = random.choice(
+        device_lookup[user_id]
+    )
 
     # Random Merchant
-    merchant = merchants.sample(1).iloc[0]
+    merchant = random.choice(
+        merchant_list
+    )
 
     # Random Location
-    location = locations.sample(1).iloc[0]
+    location = random.choice(
+        location_list
+    )
 
     previous_balance = user_balance[user_id]
 
@@ -91,23 +106,81 @@ def generate_transaction():
 
     user_balance[user_id] = current_balance
 
+    # -------------------------
+    # Fraud Logic
+    # -------------------------
+
+    risk = 0
+
+    is_new_device = False
+    is_new_location = False
+
+    # Very high transaction
+    if amount > avg_amount * 5:
+        risk += 40
+
+    # New device
+    if random.random() < 0.10:
+        is_new_device = True
+        risk += 25
+
+    # New location
+    if random.random() < 0.08:
+        is_new_location = True
+        risk += 20
+
+    # High-risk merchant
+    if merchant["MerchantRiskLevel"] == "High":
+        risk += 20
+
+    elif merchant["MerchantRiskLevel"] == "Medium":
+        risk += 10
+
+    # High-risk country
+    if location["RiskLevel"] == "High":
+        risk += 20
+
+    elif location["RiskLevel"] == "Medium":
+        risk += 10
+
+    # Previous frauds
+    risk += user["PreviousFraudCount"] * 15
+
+    # Credit score
+    if user["CreditScore"] < 550:
+        risk += 15
+
+    # Final decision
+    is_fraud = risk >= 60
+
+    timestamp = fake.date_time_between(
+        start_date="-1y",
+        end_date="now"
+    )
+
     return {
-        "TransactionID": str(uuid.uuid4()),
+        "TransactionID": f"TXN{uuid.uuid4().hex[:12].upper()}",
+        "CreditScore": user["CreditScore"],
+        "PreviousFraudCount": user["PreviousFraudCount"],
         "UserID": user_id,
         "MerchantID": merchant["MerchantID"],
-        "DeviceID": device["DeviceID"],
-        "LocationID": location["LocationID"],
+        "MerchantCategory": merchant["Category"],
+        "MerchantRisk": merchant["MerchantRiskLevel"],
+        "DeviceType": device["DeviceType"],
+        "City": location["City"],
+        "Country": location["Country"],
+        "LocationRisk": location["RiskLevel"],
         "Amount": amount,
         "TransactionType": random.choice(TRANSACTION_TYPES),
-        "Timestamp": fake.date_time_between(
-            start_date="-1y",
-            end_date="now"
-        ),
+        "Timestamp": timestamp,
+        "HourOfDay": timestamp.hour,
+        "DayOfWeek": timestamp.strftime("%A"),
         "PreviousBalance": previous_balance,
         "CurrentBalance": current_balance,
-        "IsNewDevice": False,
-        "IsNewLocation": False,
-        "FraudLabel": 0
+        "RiskScore": risk,
+        "IsNewDevice": is_new_device,
+        "IsNewLocation": is_new_location,
+        "FraudLabel": int(is_fraud)
     }
 
 # =========================================
@@ -124,6 +197,16 @@ transactions_df = pd.DataFrame(transactions)
 transactions_df.to_csv(
     "output/transactions.csv",
     index=False
+)
+
+print("\nFraud Distribution:")
+
+print(transactions_df["FraudLabel"].value_counts())
+
+print()
+
+print(
+    transactions_df["FraudLabel"].value_counts(normalize=True) * 100
 )
 
 print(transactions_df.head())
